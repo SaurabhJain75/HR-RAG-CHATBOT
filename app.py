061ec36d139cf350fc7aa12ba9e1de5720977980
SaurabhJain75 <saurabh2002.sj@gmail.com>
2026-05-18 05:08:22 +0530

    fix: Made some changes to make ui better

--- app.py
+++ app.py
@@ -464,7 +464,7 @@ padding-top: 0rem !important;
     bottom: 0 !important;
     # left: 0 !important;  ====
     right: 0 !important;
-    # padding: 0.875rem 2rem 1.25rem !important; =====
+    # padding: 0.875rem 2rem 1.25rem !important; ===
     background: linear-gradient(to top, var(--bg) 65%, transparent) !important;
     z-index: 999 !important;
     display: flex !important;
